5405fdd42806d2f518f028bc33cda459969a3ae2
Sayak Chakraborty <sayak21chakraborty@gmail.com>
2023-03-08 23:25:43 +0530

    Second Commit

--- src/main/scala/scala/strings/ScalaStrings.scala
+++ src/main/scala/scala/strings/ScalaStrings.scala
@@ -1,5 +1,12 @@
 package scala.strings
 
 object ScalaStrings {
+  val str1 : String = "Sayak Chakraborty"
+  val str2 = " is my name"
+
+  def main(args: Array[String]): Unit = {
+      println(str1.length())
+      println(str1.concat(str2))
+  }
 
 }
